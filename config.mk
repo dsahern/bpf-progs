ed57261013f5e70d9eab026f06b1048133271705
David Ahern <dsahern@gmail.com>
2020-04-11 15:01:34 +0000

    config: Comment out KVER
    Committed by mistake. Default should be to detect based on running
    kernel.
    
    Signed-off-by: David Ahern <dsahern@gmail.com>

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@
 #KDIR=/lib/modules/$(KVER)/source
 #KDIR=/usr/src/linux-headers-$(KVER)/
 #BDIR=/lib/modules/$(KVER)/build
-KVER=4.14.91-1-generic
+#KVER=4.14.91-1-generic
 
 # Where to find libbpf and include files. Default is
 # to use the install from github repo to ./libbpf.
