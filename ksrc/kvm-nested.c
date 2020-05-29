e69a6cdd678f9be41fa4d9c5fbf4167b962660a0
David Ahern <dsahern@gmail.com>
2020-05-29 20:41:25 +0000

    Add command to monitor tasks doing nested virtualization
    Signed-off-by: David Ahern <dsahern@gmail.com>

---
+++ ksrc/kvm-nested.c
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-2.0
+/* track which processes are doing nested virt
+ * David Ahern <dsahern@gmail.com>
+ */
+
+#define KBUILD_MODNAME "kvm_nested"
+#include <uapi/linux/bpf.h>
+#include <linux/version.h>
+#include <bpf/bpf_helpers.h>
+
+struct bpf_map_def SEC("maps") nested_virt_map = {
+        .type = BPF_MAP_TYPE_HASH,
+        .key_size = sizeof(u64),
+        .value_size = sizeof(u64),
+        .max_entries = 512,
+};
+
+SEC("tracepoint/kvm/kvm_nested_vmexit")
+int bpf_nested_kvm(void *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	u64 *entry;
+
+	entry = bpf_map_lookup_elem(&nested_virt_map, &pid);
+	if (entry) {
+		__sync_fetch_and_add(entry, 1);
+	} else {
+		u64 val = 1;
+
+		bpf_map_update_elem(&nested_virt_map, &pid, &val, BPF_ANY);
+	}
+
+	return 0;
+}
+
+SEC("tracepoint/sched/sched_process_exit")
+int bpf_sched_exit(void *ctx)
+{
+	u64 pid = bpf_get_current_pid_tgid();
+	u64 *entry;
+
+	entry = bpf_map_lookup_elem(&nested_virt_map, &pid);
+	if (entry)
+		bpf_map_delete_elem(&nested_virt_map, &pid);
+
+	return 0;
+}
+
+char _license[] SEC("license") = "GPL";
+int _version SEC("version") = LINUX_VERSION_CODE;
---
+++ src/kvm-nested.c
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Dump users of kvm-nested virt
+ *
+ * Copyright (c) 2020 David Ahern <dsahern@gmail.com>
+ */
+#include <linux/bpf.h>
+#include <ctype.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <libgen.h>
+#include <locale.h>
+#include <bpf/bpf.h>
+
+#include "libbpf_helpers.h"
+#include "perf_events.h"
+#include "timestamps.h"
+
+#define MAX_CPUS        128
+
+struct data {
+	__u64 time;
+	__u32 cpu;
+};
+
+#include "perf_events.c"
+
+bool done;
+
+static void process_event(struct data *data)
+{
+	/* nothing to do */
+}
+
+static int dump_map(int map_fd)
+{
+	__u64 *key, *prev_key = NULL, val, pid;
+	char buf[64];
+	int err;
+
+	key = calloc(1, sizeof(*key));
+	if (!key) {
+		fprintf(stderr, "Failed to allocate memory for key\n");
+		return 1;
+	}
+
+	printf("\n%s:\n", timestamp(buf, sizeof(buf), 0));
+	while(1) {
+		err = bpf_map_get_next_key(map_fd, prev_key, key);
+		if (err) {
+			if (errno == ENOENT)
+				err = 0;
+			break;
+		}
+
+		val = 0;
+		if (!bpf_map_lookup_elem(map_fd, key, &val)) {
+			pid = *key;
+			printf("    tgid %u pid %u count %llu\n",
+				(__u32)(pid >> 32), (__u32)pid, val);
+		}
+
+		prev_key = key;
+	}
+
+	free(key);
+	return err;
+}
+
+static void sig_handler(int signo)
+{
+	printf("Terminating by signal %d\n", signo);
+	done = true;
+}
+
+static void print_usage(char *prog)
+{
+	printf(
+	"usage: %s OPTS\n\n"
+	"	-f bpf-file    bpf filename to load\n"
+	"	-t rate        time rate (seconds) to dump stats\n"
+	, basename(prog));
+}
+
+int main(int argc, char **argv)
+{
+	struct bpf_prog_load_attr prog_load_attr = { };
+	char *objfile = "kvm-nested.o";
+	const char *tps[] = {
+		"kvm/kvm_nested_vmexit",
+		"sched/sched_process_exit",
+		NULL
+	};
+	bool filename_set = false;
+	struct bpf_object *obj;
+	int display_rate = 10;
+	struct bpf_map *map;
+	int rc, tmp;
+	int map_fd;
+
+	while ((rc = getopt(argc, argv, "f:t:")) != -1)
+	{
+		switch(rc) {
+		case 'f':
+			objfile = optarg;
+			filename_set = true;
+			break;
+		case 't':
+			tmp = atoi(optarg);
+			if (!tmp) {
+				fprintf(stderr, "Invalid display rate\n");
+				return 1;
+			}
+			display_rate = tmp;
+			break;
+		default:
+			print_usage(argv[0]);
+			return 1;
+		}
+	}
+
+	if (signal(SIGINT, sig_handler) ||
+	    signal(SIGHUP, sig_handler) ||
+	    signal(SIGTERM, sig_handler)) {
+		perror("signal");
+		return 1;
+	}
+
+	setlinebuf(stdout);
+	setlinebuf(stderr);
+
+	if (load_obj_file(&prog_load_attr, &obj, objfile, filename_set))
+		return 1;
+
+	map = bpf_object__find_map_by_name(obj, "nested_virt_map");
+	if (!map) {
+		printf("Failed to get map in obj file\n");
+		return 1;
+	}
+	map_fd = bpf_map__fd(map);
+
+	rc = 1;
+	if (do_tracepoint(obj, tps))
+		goto out;
+
+	rc = 0;
+	while (!done) {
+		sleep(display_rate);
+		if (dump_map(map_fd))
+			break;
+	}
+
+out:	
+	return rc;
+}
