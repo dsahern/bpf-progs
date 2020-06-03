6699323bf5a17630c3e122dcbb90d0a2556d5772
David Ahern <dsahern@gmail.com>
2020-06-03 19:35:38 +0000

    net_rx_action: Lower bucket sizes
    Higher ones were needed because of sfc and ovs bugs causing huge
    spikes in time to process packets.
    
    Signed-off-by: David Ahern <dsahern@gmail.com>

--- include/net_rx_action.h
+++ include/net_rx_action.h
@@ -4,15 +4,15 @@
 
 #define MAX_CPUS	128
 
-#define NET_RX_BUCKET_0    100
-#define NET_RX_BUCKET_1    500
-#define NET_RX_BUCKET_2   1000
-#define NET_RX_BUCKET_3   2500
-#define NET_RX_BUCKET_4   5000
-#define NET_RX_BUCKET_5  10000
-#define NET_RX_BUCKET_6  20000
-#define NET_RX_BUCKET_7  50000
-#define NET_RX_BUCKET_8 100000
+#define NET_RX_BUCKET_0     5
+#define NET_RX_BUCKET_1    10
+#define NET_RX_BUCKET_2    25
+#define NET_RX_BUCKET_3    50
+#define NET_RX_BUCKET_4   100
+#define NET_RX_BUCKET_5   500
+#define NET_RX_BUCKET_6  1000
+#define NET_RX_BUCKET_7  2000
+#define NET_RX_BUCKET_8  5000
 
 /* bucket 9 is anything > than bucket 8 */
 /* bucket 10 is errors */
