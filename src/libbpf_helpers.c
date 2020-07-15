bf9e39737b0db7661ff63ebdabc60a1bd68e5fe5
David Ahern <dsahern@gmail.com>
2020-07-15 17:18:48 +0000

    libbpf_helpers: Add lookups by name and path
    Add libbpf wrappers to get map and program fd by name and path.
    
    Signed-off-by: David Ahern <dsahern@gmail.com>

--- src/libbpf_helpers.c
+++ src/libbpf_helpers.c
@@ -56,6 +56,107 @@ int load_obj_file(struct bpf_prog_load_attr *attr,
 	return 1;
 }
 
+int bpf_map_get_fd_by_name(const char *name)
+{
+	struct bpf_map_info info = {};
+	__u32 len = sizeof(info);
+	__u32 id = 0;
+	int err, fd;
+
+	while (1) {
+		err = bpf_map_get_next_id(id, &id);
+		if (err)
+			break;
+
+		fd = bpf_map_get_fd_by_id(id);
+		if (fd < 0)
+			continue;
+
+		err = bpf_obj_get_info_by_fd(fd, &info, &len);
+		if (!err && strcmp(info.name, name) == 0)
+			return fd;
+
+		close(fd);
+	}
+
+	return -1;
+}
+
+/* from bpftool */
+static int get_fd_type(int fd)
+{
+	char path[PATH_MAX];
+	char buf[512];
+	ssize_t n;
+
+	snprintf(path, sizeof(path), "/proc/self/fd/%d", fd);
+
+	n = readlink(path, buf, sizeof(buf));
+	if (n < 0) {
+		fprintf(stderr, "can't read link type: %s\n", strerror(errno));
+		return -1;
+	}
+	if (n == sizeof(path)) {
+		fprintf(stderr, "can't read link type: path too long!\n");
+		return -1;
+	}
+
+	if (strstr(buf, "bpf-map"))
+		return BPF_OBJ_MAP;
+
+	if (strstr(buf, "bpf-prog"))
+		return BPF_OBJ_PROG;
+
+	if (strstr(buf, "bpf-link"))
+		return BPF_OBJ_LINK;
+
+	return BPF_OBJ_UNKNOWN;
+}
+
+int bpf_map_get_fd_by_path(const char *path)
+{
+	enum bpf_obj_type objtype;
+	int fd;
+
+	fd = bpf_obj_get(path);
+	if (fd < 0) {
+		fprintf(stderr, "Failed to get bpf object (%s): %s\n",
+			path, strerror(errno));
+		return -1;
+	}
+
+	objtype = get_fd_type(fd);
+	if (objtype != BPF_OBJ_MAP) {
+		fprintf(stderr, "Path is not to a BPF map\n");
+		close(fd);
+		return -1;
+	}
+
+	return fd;
+}
+
+int bpf_prog_get_fd_by_path(const char *path)
+{
+	enum bpf_obj_type objtype;
+	int fd;
+
+	fd = bpf_obj_get(path);
+	if (fd < 0) {
+		fprintf(stderr, "Failed to get bpf object (%s): %s\n",
+			path, strerror(errno));
+		return -1;
+	}
+
+	objtype = get_fd_type(fd);
+	if (objtype != BPF_OBJ_PROG) {
+		fprintf(stderr, "Path is not to a BPF program\n");
+		close(fd);
+		return -1;
+	}
+
+	return fd;
+}
+
 int attach_to_dev_generic(int idx, int prog_fd, const char *dev)
 {
 	int err;
@@ -104,4 +205,3 @@ int detach_from_dev(int idx, const char *dev)
 
 	return 0;
 }
-
--- src/libbpf_helpers.h
+++ src/libbpf_helpers.h
@@ -4,9 +4,22 @@
 
 #include <bpf/libbpf.h>
 
+enum bpf_obj_type {
+	BPF_OBJ_UNKNOWN,
+	BPF_OBJ_PROG,
+	BPF_OBJ_MAP,
+	BPF_OBJ_LINK,
+	BPF_OBJ_BTF,
+};
+
 int load_obj_file(struct bpf_prog_load_attr *attr,
                   struct bpf_object **obj,
                   const char *objfile, bool user_set);
+
+int bpf_map_get_fd_by_name(const char *name);
+int bpf_map_get_fd_by_path(const char *path);
+
+int bpf_prog_get_fd_by_path(const char *path);
 
 int attach_to_dev_generic(int idx, int prog_fd, const char *dev);
 int detach_from_dev_generic(int idx, const char *dev);
