6e5fc61dcde991941f95d6d401de015726240b2a
David Ahern <dsahern@gmail.com>
2020-08-21 13:53:06 -0600

    acl: move the acl checks in drop_packet to helper
    Move acl checks in drop_packet to a helper that is only
    invoked if the map is non-NULL.
    
    Signed-off-by: David Ahern <dsahern@gmail.com>

--- ksrc/acl_vm_common.h
+++ ksrc/acl_vm_common.h
@@ -37,53 +37,11 @@ static __always_inline bool ipv6_any(const struct in6_addr *a1)
 	       a1->s6_addr32[3] == 0;
 }
 
-/* returns true if packet should be dropped; false to continue */
-static __always_inline bool drop_packet(void *data, void *data_end,
-					struct vm_info *vi, bool rx,
-					struct flow *fl,
-					struct bpf_map_def *acl_map)
+static __always_inline bool check_acl(struct ethhdr *eth, struct flow *fl,
+				      struct bpf_map_def *acl_map)
 {
-	struct ethhdr *eth = data;
 	struct acl_key key = {};
 	struct acl_val *val;
-	void *nh = eth + 1;
-	u16 h_proto;
-	int rc;
-
-	if (nh > data_end)
-		return true;
-
-	/* direction: Tx = to VM, Rx = from VM */
-	if (!mac_cmp(vi->mac, rx ? eth->h_source : eth->h_dest))
-		return true;
-
-	h_proto = eth->h_proto;
-#ifdef SUPPORT_QINQ
-	if (h_proto == htons(ETH_P_8021AD)) {
-		struct vlan_hdr *vhdr;
-
-		vhdr = nh;
-		if (vhdr + 1 > data_end)
-			return true;
-
-		nh += sizeof(*vhdr);
-		h_proto = vhdr->h_vlan_encapsulated_proto;
-	}
-#endif
-	if (h_proto == htons(ETH_P_8021Q)) {
-		struct vlan_hdr *vhdr;
-
-		vhdr = nh;
-		if (vhdr + 1 > data_end)
-			return true;
-
-		nh += sizeof(*vhdr);
-		h_proto = vhdr->h_vlan_encapsulated_proto;
-	}
-
-	rc = parse_pkt(fl, h_proto, nh, data_end, 0);
-	if (rc)
-		return rc > 0 ? false : true;
 
 	key.protocol = fl->protocol;
 	if (key.protocol == IPPROTO_TCP || key.protocol == IPPROTO_UDP)
@@ -147,5 +105,59 @@ static __always_inline bool drop_packet(void *data, void *data_end,
 	if (val->port && val->port == fl->ports.sport)
 		return true;
 
-	return true;
+	return key.port ? true : false;
+}
+
+/* returns true if packet should be dropped; false to continue */
+static __always_inline bool drop_packet(void *data, void *data_end,
+					struct vm_info *vi, bool rx,
+					struct flow *fl,
+					struct bpf_map_def *acl_map)
+{
+	struct ethhdr *eth = data;
+	void *nh = eth + 1;
+	bool rc = false;
+	u16 h_proto;
+	int ret;
+
+
+	if (nh > data_end)
+		return true;
+
+	/* direction: Tx = to VM, Rx = from VM */
+	if (!mac_cmp(vi->mac, rx ? eth->h_source : eth->h_dest))
+		return true;
+
+	h_proto = eth->h_proto;
+#ifdef SUPPORT_QINQ
+	if (h_proto == htons(ETH_P_8021AD)) {
+		struct vlan_hdr *vhdr;
+
+		vhdr = nh;
+		if (vhdr + 1 > data_end)
+			return true;
+
+		nh += sizeof(*vhdr);
+		h_proto = vhdr->h_vlan_encapsulated_proto;
+	}
+#endif
+	if (h_proto == htons(ETH_P_8021Q)) {
+		struct vlan_hdr *vhdr;
+
+		vhdr = nh;
+		if (vhdr + 1 > data_end)
+			return true;
+
+		nh += sizeof(*vhdr);
+		h_proto = vhdr->h_vlan_encapsulated_proto;
+	}
+
+	ret = parse_pkt(fl, h_proto, nh, data_end, 0);
+	if (ret)
+		return ret > 0 ? false : true;
+
+	if (acl_map)
+		rc = check_acl(eth, fl, acl_map);
+
+	return rc;
 }
