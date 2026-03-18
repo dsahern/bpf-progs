14cd457d9a6d1d1e49602107a34b1cc5d52cb2bd
David Ahern <dsahern@gmail.com>
2026-03-18 16:55:43 -0600

    hist: Add print factor to shorten range buckets
    more pleasing when printing kB to GB
    
    Signed-off-by: David Ahern <dsahern@gmail.com>

--- src/hist.c
+++ src/hist.c
@@ -4,12 +4,15 @@
 
 #include "hist.h"
 
-int hist_init(__u64 *ranges, __u32 num_buckets, struct hist *h)
+int hist_init(__u64 *ranges, __u32 num_buckets, struct hist *h, __u64 print_factor)
 {
 	int i;
 
 	if (num_buckets < 1)
 		return -EINVAL;
+
+	/* must be 1 or higher */
+	h->print_factor = print_factor ? : 1;
 
 	/* + 1 is for values > max range */
 	h->num_buckets = num_buckets + 1;
@@ -46,6 +49,8 @@ void hist_update(struct hist *h, __u64 val)
 {
 	int i;
 
+	h->entries++;
+
 	for (i = 0; i < h->num_buckets - 1; ++i) {
 		if (val <= h->ranges[i]) {
 			h->buckets[i]++;
@@ -58,12 +63,23 @@ void hist_update(struct hist *h, __u64 val)
 
 void hist_print(struct hist *h)
 {
+	__u64 rl, rh;
 	__u32 i;
 
-	printf("   %'10llu  -> %'10llu:   %'8llu\n", 0ULL, h->ranges[0], h->buckets[0]);
+	rl = h->ranges[0];
+	if (h->print_factor > 1)
+		rl /= h->print_factor;
 
-	for (i = 1; i < h->num_buckets - 1; ++i)
-		printf("   %'10llu+ -> %'10llu:   %'8llu\n", h->ranges[i-1], h->ranges[i], h->buckets[i]);
+	printf("   %'10llu  -> %'10llu:   %'8llu\n", 0ULL, rl, h->buckets[0]);
 
-	printf("   %'10llu+ -> %10s:   %'8llu\n", h->ranges[i-1], "up", h->buckets[i]);
+	for (i = 1; i < h->num_buckets - 1; ++i) {
+		rh = h->ranges[i];
+		if (h->print_factor > 1)
+			rh /= h->print_factor;
+
+		printf("   %'10llu+ -> %'10llu:   %'8llu\n", rl, rh, h->buckets[i]);
+		rl = rh;
+	}
+
+	printf("   %'10llu+ -> %10s:   %'8llu\n", rl, "up", h->buckets[i]);
 }
--- src/hist.h
+++ src/hist.h
@@ -5,9 +5,11 @@
 struct hist {
 	__u64 *buckets;
 	__u64 *ranges;
+	__u64 entries;
+	__u64 print_factor;
 	__u32 num_buckets;
 };
 
-int hist_init(__u64 *ranges, __u32 num_buckets, struct hist *h);
+int hist_init(__u64 *ranges, __u32 num_buckets, struct hist *h, __u64 print_factor);
 void hist_update(struct hist *h, __u64 val);
 void hist_print(struct hist *h);
