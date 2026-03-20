4a48558b35e3fde7e6f249eb3f3191d6e819343e
David Ahern <dsahern@gmail.com>
2026-03-20 16:39:08 -0600

    hist: Drop the print_factor in favor of normalized units
    Signed-off-by: David Ahern <dsahern@gmail.com>

--- src/hist.c
+++ src/hist.c
@@ -1,48 +1,153 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 
 #include "hist.h"
+#include "timestamps.h"
 
-int hist_init(__u64 *ranges, __u32 num_buckets, struct hist *h, __u64 print_factor)
+static void hist_set_print_bytes(struct hist *h)
 {
 	int i;
+
+	for (i = 0; i < h->num_buckets - 1; ++i) {
+		if (h->ranges[i] >= SIZE_TB(1)) {
+			h->ranges_print[i] = h->ranges[i] / SIZE_TB(1);
+			h->ranges_unit[i] = "TB";
+		} else if (h->ranges[i] >= SIZE_GB(1)) {
+			h->ranges_print[i] = h->ranges[i] / SIZE_GB(1);
+			h->ranges_unit[i] = "GB";
+		} else if (h->ranges[i] >= SIZE_MB(1)) {
+			h->ranges_print[i] = h->ranges[i] / SIZE_MB(1);
+			h->ranges_unit[i] = "MB";
+		} else if (h->ranges[i] >= SIZE_KB(1)) {
+			h->ranges_print[i] = h->ranges[i] / SIZE_KB(1);
+			h->ranges_unit[i] = "KB";
+		} else {
+			h->ranges_print[i] = h->ranges[i];
+			h->ranges_unit[i] = "B";
+		}
+	}
+
+	/* last bucket */
+	h->ranges_unit[i] = h->ranges_unit[i-1];
+}
+
+static void hist_set_print_rate(struct hist *h)
+{
+	int i;
+
+	for (i = 0; i < h->num_buckets - 1; ++i) {
+		if (h->ranges[i] >= SIZE_TB(1)) {
+			h->ranges_print[i] = h->ranges[i] / SIZE_TB(1);
+			h->ranges_unit[i] = "TB/sec";
+		} else if (h->ranges[i] >= SIZE_GB(1)) {
+			h->ranges_print[i] = h->ranges[i] / SIZE_GB(1);
+			h->ranges_unit[i] = "GB/sec";
+		} else if (h->ranges[i] >= SIZE_MB(1)) {
+			h->ranges_print[i] = h->ranges[i] / SIZE_MB(1);
+			h->ranges_unit[i] = "MB/sec";
+		} else if (h->ranges[i] >= SIZE_KB(1)) {
+			h->ranges_print[i] = h->ranges[i] / SIZE_KB(1);
+			h->ranges_unit[i] = "KB/sec";
+		} else {
+			h->ranges_print[i] = h->ranges[i];
+			h->ranges_unit[i] = "B/sec";
+		}
+	}
+
+	/* last bucket */
+	h->ranges_unit[i] = h->ranges_unit[i-1];
+}
+
+static void hist_set_print_nsec(struct hist *h)
+{
+	int i;
+
+	for (i = 0; i < h->num_buckets - 1; ++i) {
+		if (h->ranges[i] >= NSEC_PER_SEC) {
+			h->ranges_print[i] = h->ranges[i] / NSEC_PER_SEC;
+			h->ranges_unit[i] = "sec";
+		} else if (h->ranges[i] >= NSEC_PER_MSEC) {
+			h->ranges_print[i] = h->ranges[i] / NSEC_PER_MSEC;
+			h->ranges_unit[i] = "msec";
+		} else if (h->ranges[i] >= NSEC_PER_USEC) {
+			h->ranges_print[i] = h->ranges[i] / NSEC_PER_USEC;
+			h->ranges_unit[i] = "usec";
+		} else {
+			h->ranges_print[i] = h->ranges[i];
+			h->ranges_unit[i] = "nsec";
+		}
+	}
+
+	/* last bucket */
+	h->ranges_unit[i] = h->ranges_unit[i-1];
+}
+
+int hist_init(struct hist *h, __u64 *ranges, __u32 num_buckets, 
+	      enum hist_units units)
+{
+	int i, rc;
 
 	if (num_buckets < 1)
 		return -EINVAL;
 
-	/* must be 1 or higher */
-	h->print_factor = print_factor ? : 1;
+	h->units = units;
 
 	/* + 1 is for values > max range */
 	h->num_buckets = num_buckets + 1;
 
-	h->buckets = calloc(num_buckets + 1, sizeof(__u64));
-	h->ranges = calloc(num_buckets + 1, sizeof(__u64));
+	h->buckets = calloc(num_buckets + 1, sizeof(*h->buckets));
+	h->ranges = calloc(num_buckets + 1, sizeof(*h->ranges));
+	h->ranges_print = calloc(num_buckets + 1, sizeof(*h->ranges_print));
+	h->ranges_unit = calloc(num_buckets + 1, sizeof(*h->ranges_unit));
 
-	if (!h->buckets || !h->ranges) {
-		free(h->buckets);
-		free(h->ranges);
-		h->num_buckets = 0;
-		return -ENOMEM;
+	if (!h->buckets || !h->ranges ||
+	    !h->ranges_print || !h->ranges_unit) {
+		rc = -ENOMEM;
+		goto err_out;
 	}
 
 	h->ranges[0] = ranges[0];
 	for (i = 1; i < num_buckets; ++i) {
 		if (ranges[i] < ranges[i-1]) {
-			free(h->buckets);
-			free(h->ranges);
-			h->buckets = NULL;
-			h->ranges = NULL;
-			h->num_buckets = 0;
-
-			return -EINVAL;
+			rc = -EINVAL;
+			goto err_out;
 		}
 
 		h->ranges[i] = ranges[i];
 	}
 
+	switch (h->units) {
+	case HIST_UNITS_BYTES:
+		hist_set_print_bytes(h);
+		break;
+	case HIST_UNITS_BYTES_PER_SEC:
+		hist_set_print_rate(h);
+		break;
+	case HIST_UNITS_NSEC:
+		hist_set_print_nsec(h);
+		break;
+	}
+
 	return 0;
+err_out:
+	free(h->buckets);
+	free(h->ranges);
+	free(h->ranges_print);
+	free(h->ranges_unit);
+	memset(h, 0, sizeof(*h));
+
+	return rc;
+}
+
+void hist_cleanup(struct hist *h)
+{
+	free(h->buckets);
+	free(h->ranges);
+	free(h->ranges_print);
+	free(h->ranges_unit);
+	memset(h, 0, sizeof(*h));
 }
 
 void hist_update(struct hist *h, __u64 val)
@@ -63,23 +168,11 @@ void hist_update(struct hist *h, __u64 val)
 
 void hist_print(struct hist *h)
 {
-	__u64 rl, rh;
-	__u32 i;
+	__u32 i = 0;
 
-	rl = h->ranges[0];
-	if (h->print_factor > 1)
-		rl /= h->print_factor;
-
-	printf("   %'10llu  -> %'10llu:   %'8llu\n", 0ULL, rl, h->buckets[0]);
-
-	for (i = 1; i < h->num_buckets - 1; ++i) {
-		rh = h->ranges[i];
-		if (h->print_factor > 1)
-			rh /= h->print_factor;
-
-		printf("   %'10llu+ -> %'10llu:   %'8llu\n", rl, rh, h->buckets[i]);
-		rl = rh;
+	for (i = 0; i < h->num_buckets-1; ++i) {
+		printf("%7s %-6s : %'8llu\n", "", "", h->buckets[i]);
+		printf("%7.1f %-6s :\n", h->ranges_print[i], h->ranges_unit[i]);
 	}
-
-	printf("   %'10llu+ -> %10s:   %'8llu\n", rl, "up", h->buckets[i]);
+	printf("%7s %-6s : %'8llu\n", "", "", h->buckets[i]);
 }
--- src/hist.h
+++ src/hist.h
@@ -1,15 +1,34 @@
 #pragma once
 
 #include <linux/types.h>
+#include <stdbool.h>
 
-struct hist {
-	__u64 *buckets;
-	__u64 *ranges;
-	__u64 entries;
-	__u64 print_factor;
-	__u32 num_buckets;
+#define SIZE_B(x)     (x)
+#define SIZE_KB(x)   ((x) * 1024ULL)
+#define SIZE_MB(x)   ((x) * 1024ULL * 1024ULL)
+#define SIZE_GB(x)   ((x) * 1024ULL * 1024ULL * 1024ULL)
+#define SIZE_TB(x)   ((x) * 1024ULL * 1024ULL * 1024ULL * 1024ULL)
+
+enum hist_units {
+	HIST_UNITS_BYTES,
+	HIST_UNITS_BYTES_PER_SEC,
+	HIST_UNITS_NSEC,
 };
 
-int hist_init(__u64 *ranges, __u32 num_buckets, struct hist *h, __u64 print_factor);
+struct hist {
+	__u64 entries;
+	__u64 *buckets;
+	__u64 *ranges;
+
+	float *ranges_print;
+	char **ranges_unit;
+
+	__u32 num_buckets;
+	enum hist_units units;
+};
+
+int hist_init(struct hist *h, __u64 *ranges, __u32 num_buckets,
+	      enum hist_units);
+void hist_cleanup(struct hist *h);
 void hist_update(struct hist *h, __u64 val);
 void hist_print(struct hist *h);
